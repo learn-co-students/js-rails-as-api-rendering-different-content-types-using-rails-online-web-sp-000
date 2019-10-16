8ed87f1f67a35b435a8b58855dc3777138e1374e
Aliciea Dawn Suechin <aliciea@Alicieas-MacBook-Pro.local>
2019-10-16 12:15:45 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,55 @@
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render 'birds/index.html.erb'
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render plain: "Hello #{@birds[3].name}"
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: { message: 'Hashes of data will get converted to JSON' }
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: ['As','well','as','arrays']
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: @birds
+#   end
+# end
+
+# class BirdsController < ApplicationController
+#   def index
+#     @birds = Bird.all
+#     render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }
+#   end
+# end
+
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
   end
 end
