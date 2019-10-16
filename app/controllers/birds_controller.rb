2a96330e5cb0c074e59a61f43e34b90ea9d8cc06
Tsunamins <reillyamr@gmail.com>
2019-10-16 17:22:29 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,12 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
-  end
+
+    #no longer need instance variables @birds when rendering to json
+    birds = Bird.all
+
+    #@birds = Bird.all
+    #render json: @birds
+    #more detailed nesting:
+    render json: { birds: birds, messages: ['Hello Birds', 'Goodbye Birds'] } #added a messages key to end of birds key
+   end
 end
