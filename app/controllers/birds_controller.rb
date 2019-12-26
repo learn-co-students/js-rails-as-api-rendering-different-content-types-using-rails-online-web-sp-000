51a84e668712ebe283d6aaae603405307ee4db72
github username <github email address>
2019-12-25 23:42:51 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,33 @@
 class BirdsController < ApplicationController
+  
+  # def index
+  #   @birds = Bird.all
+  #   render plain: 'birds/index.html.erb'
+  # end
+
+  # def index
+  #   @birds = Bird.all
+  #   render plain: "Hello #{@birds[3].name}"
+  # end
+
+  # def index
+  #   @birds = Bird.all
+  #   render json: { message: 'Hashes of data will get converted to JSON' }
+  # end
+
+  # def index
+  #   @birds = Bird.all
+  #   render json: ['As','well','as','arrays']
+  # end
+
+  # def index
+  #   @birds = Bird.all
+  #   render json: @birds
+  # end
+
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+    birds = Bird.all
+    render json: birds
   end
+
 end
