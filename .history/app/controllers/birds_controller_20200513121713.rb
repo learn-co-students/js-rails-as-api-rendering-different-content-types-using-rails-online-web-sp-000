cc5418fa8d35019d274608179997bb3d6d75ff90
Lu Daley <46202132+DevLuDaley@users.noreply.github.com>
2020-05-13 12:27:08 -0400

    Done.

--- .history/app/controllers/birds_controller_20200513121713.rb
+++ .history/app/controllers/birds_controller_20200513121713.rb
@@ -1,15 +1,10 @@
 class BirdsController < ApplicationController
   def index
-    birds = Bird.all
+    @birds = Bird.all
     # render 'birds/index.html.erb'
     # render plain: "hello #{@birds[3].name}"
     # render json: 'Remember that JSON is just object notation converted to string data, so strings also work here'
     # render json: { message: 'Hashes of data will get converted to JSON' }
-    # render json: ['As','well','as','arrays']
-    # render json: @birds
-    # render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] } 
-    # render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] }.to_json
-    #! render doesn't need instance variables
-    render json: birds
+    render json: { message: 'Hashes of data will get converted to JSON' }
   end
 end
