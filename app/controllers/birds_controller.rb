4d6e72553a5d2656159888957618ec41c6038fac
github username <github email address>
2020-09-17 18:09:41 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,20 @@
+#RAILS CAN RENDER JSON! INDEPENDENT JAVASCRIPT FRONTENDS THAT COMMUNICATE W RAILS BACKENDS!
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
-  end
-end
+   # @birds = Bird.all no more instance variables! 
+   birds = Bird.all
+    #render json: @birds #rendering json from a rails controller, but instance variables are no longer needed, only when rendering to ERB
+   # render json: { birds: @birds, messages: ['Hello Birds', 'Goodbye Birds'] } #detailed nesting
+   render json: birds #local variable!
+  end 
+end 
+
+
+
+
+#index aaction rendered all birds
+
+#render plain: "Hello #{@birds[3].name}" renders Hello Mourning Dove in the browser
+
+#adding .to_json at the end will convert our array to a hash 
+
