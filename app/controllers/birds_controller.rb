bebab472863eb3c3472867bfdec5239d7d52f544
Technicallyaustin <Technicallyaustin@gmail.com>
2021-06-18 13:45:04 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,11 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds
   end
+
+  
+  ## fetch("http://localhosT:3000/birds")
+##  .then(response => response.json())
+##  .then(json => console.log(json))
 end
